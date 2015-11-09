87822b58c89c96c3499181901f9c3c26d144808c
liuxd <liuxd@fruitday.com>
2015-11-09 15:52:41 +0800

    refactor

--- test.php
+++ test.php
@@ -5,6 +5,7 @@ require __DIR__ . '/util.php';
 require __DIR__ . '/SDK/Agent.php';
 
 $agent = new \weixin\Agent('User', 'wx1061e4e55dd6de25', '9dbfb0f945333b1c141cbc215aa734c3');
-$agent->User->getUserInfo($_GET['code']);
+$aUserInfo = $agent->User->getUserInfo($_GET['code']);
+c($aUserInfo);
 
 # end of this file

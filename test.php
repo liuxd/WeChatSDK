c24efb205080a036038679e41f9f23f27eea6d84
liuxd <liuxd@fruitday.com>
2015-11-09 17:31:50 +0800

    fix test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 require __DIR__ . '/util.php';
 require __DIR__ . '/SDK/Agent.php';
 
-$agent = new \weixin\Agent(['sModuleName' => 'User', 'sAppID' => $_GET['a'], 'sSecret' => $_GET['b']]);
+$agent = new Agent(['sModuleName' => 'User', 'sAppID' => $_GET['a'], 'sSecret' => $_GET['b']]);
 $aUserInfo = $agent->User->getUserInfo($_GET['code']);
 see($aUserInfo);
 

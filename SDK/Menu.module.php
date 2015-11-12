ab43f485b71511f1ef4d81da415e9c6bc06c3d93
liuxd <liuxidong1984@gmail.com>
2015-11-12 16:05:55 +0800

    add Menu.module.php and Menu->getConfig()

---
+++ SDK/Menu.module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace weixin;
+
+class Menu extends Base
+{
+	public function createConfig($aConfig)
+	{
+		//
+	}
+
+	/**
+	 * 获得自定义菜单的配置。
+	 * @return array
+	 */
+	public function getConfig()
+	{
+		$sAccessToken = $this->getGlobalAccessToken();
+		$sURL = 'https://api.weixin.qq.com/cgi-bin/get_current_selfmenu_info?access_token=' . $sAccessToken;
+		return json_decode($this->curlGet($sURL), true);
+	}
+}
+
+# end of this file
--- test.php
+++ test.php
@@ -6,6 +6,9 @@ $oRedis = new Redis;
 $oRedis->connect('127.0.0.1', 6379);
 
 $agent = new Agent(['sModuleName' => 'User', 'sAppID' => 'wx997a012f420c0ede', 'sSecret' => 'd4624c36b6795d1d99dcf0547af5443d', 'oRedis' => $oRedis]);
-print_r($agent->User->getGlobalAccessToken());
+
+$agent = new Agent(['sModuleName' => 'Menu', 'sAppID' => 'wx997a012f420c0ede', 'sSecret' => 'd4624c36b6795d1d99dcf0547af5443d', 'oRedis' => $oRedis]);
+
+print_r($agent->Menu->getConfig());
 
 # end of this file

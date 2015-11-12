97c7db5deea011740ee4b3b11d664698ba201c4e
liuxd <liuxidong1984@gmail.com>
2015-11-12 11:55:24 +0800

    use redis to store access_token

--- SDK/Base.inc.php
+++ SDK/Base.inc.php
@@ -7,6 +7,9 @@ class Base
     protected $sAppID = '';
     protected $sSecret = '';
 
+    private $sAccessTokenRedisKey = 'weixin_access_token';
+    private $oRedis = null;
+
     public function __construct($sAppID, $sSecret)
     {
         $this->sAppID = $sAppID;
@@ -15,12 +18,52 @@ class Base
 
     /**
      * 获得全局接口凭据。
-     * @return array ["access_token" => "ACCESS_TOKEN", "expires_in" => 7200]
+     * @return string
      */
     public function getGlobalAccessToken()
     {
+        $oRedis = $this->getRedis();
+        $sAccessToken = $oRedis->get($this->sAccessTokenRedisKey);
+
+        if (!empty($sAccessToken)) {
+            return $sAccessToken;
+        }
+
         $sURL = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={$this->sAppID}&secret={$this->sSecret}";
-        return json_decode($this->curlGet($sURL), true);
+        $aReturn = json_decode($this->curlGet($sURL), true);
+
+        if (isset($aReturn['access_token'])) {
+            $oRedis->set($this->sAccessTokenRedisKey, $aReturn['access_token']);
+            $oRedis->setTimeout($this->sAccessTokenRedisKey, $aReturn['expires_in']);
+        }
+
+        return $aReturn['access_token'];
+    }
+
+    private function getRedis()
+    {
+        if (!is_null($this->oRedis)) {
+            return $this->oRedis;
+        }
+
+        $sHost = '127.0.0.1';
+        $iPort = 6379;
+
+        $sConfigFile = __DIR__ . DIRECTORY_SEPARATOR . 'config.ini';
+
+        if (file_exists($sConfigFile)) {
+            $aConfig = parse_ini_file($sConfigFile, true);
+
+            if (isset($aConfig['redis'])) {
+                $sHost = $aConfig['redis']['host'];
+                $iPort = $aConfig['redis']['port'];
+            }
+        }
+
+        $this->oRedis = new \Redis;
+        $this->oRedis->connect($sHost, $iPort);
+
+        return $this->oRedis;
     }
 
     /**
